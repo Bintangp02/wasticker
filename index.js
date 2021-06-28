249583c5ea4b6c5dc83844e0606280c12d6b4c66
Bintangp02 <80300921+Bintangp02@users.noreply.github.com>
2021-06-28 17:07:41 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+			case 'stickerwm':
+		    case 'swm':
+		            pe = args.join('')
+		            var a = pe.split("|")[0];
+		            var b = pe.split("|")[1];
+		            if (isMedia && !Mel.message.videoMessage || isQuotedImage ) {
+		            const encmedia = isQuotedImage   ? JSON.parse(JSON.stringify(Mel).replace('quotedM','m')).message.extendedTextMessage.contextInfo : Mel
+		             media = await Ruri.downloadAndSaveMediaMessage(encmedia)
+		            await createExif(a,b)
+		            out = getRandom('.webp')
+		            ffmpeg(media)
+		            .on('error', (e) => {
+		            console.log(e)
+		            Ruri.sendMessage(from, 'Terjadi kesalahan', 'conversation', { quoted: Mel })
+		            fs.unlinkSync(media)
+		            })
+		            .on('end', () => {
+		            _out = getRandom('.webp')
+		            spawn('webpmux', ['-set','exif','./stik/data.exif', out, '-o', _out])
+		            .on('exit', () => {
+		            Ruri.sendMessage(from, fs.readFileSync(_out),'stickerMessage', { quoted: Mel })
+		            fs.unlinkSync(out)
+		            fs.unlinkSync(_out)
+		            fs.unlinkSync(media)
+		            })
+		            })
+		            .addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+		            .toFormat('webp')
+		            .save(out) 
+		            } else if ((isMedia && Mel.message.videoMessage.seconds < 11 || isQuotedVideo && Mel.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+		            const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(Mel).replace('quotedM','m')).message.extendedTextMessage.contextInfo : Mel
+		            const media = await Ruri.downloadAndSaveMediaMessage(encmedia)
+		            pe = args.join('')
+		            var a = pe.split("|")[0];
+		            var b = pe.split("|")[1];
+		            await createExif(a,b)
+		            out = getRandom('.webp')
+		            ffmpeg(media)
+		            .on('error', (e) => {
+		            console.log(e)
+		            Ruri.sendMessage(from, 'Terjadi kesalahan', 'conversation', { quoted: Mel })
+		            fs.unlinkSync(media)
+		            })
+		            .on('end', () => {
+		            _out = getRandom('.webp')
+		            spawn('webpmux', ['-set','exif','./stik/data.exif', out, '-o', _out])
+		            .on('exit', () => {
+		            Ruri.sendMessage(from, fs.readFileSync(_out),'stickerMessage', { quoted: Mel })
+		            fs.unlinkSync(out)
+		            fs.unlinkSync(_out)
+		            fs.unlinkSync(media)
+		            })
+		            })
+		            .addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+		            .toFormat('webp')
+		            .save(out)       
+		            } else {
+		            reply(`Kirim gambar dengan caption ${prefix}swm teks|teks atau tag gambar yang sudah dikirim`)
+		            }
+		            break
